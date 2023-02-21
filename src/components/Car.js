4c1a39f5f47965408663262fed13c315ccbefec6
BoltenCoder <moltencreeper25@gmail.com>
2023-02-20 20:52:46 -0600

    Finished

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import cars from '../cars.json'
-// Import {useParams} from "react-router-dom" here //
+import {useParams} from "react-router-dom"
 
-// import MUI components here //
-// Container, Paper, Chip //
+
+import Container from '@mui/material/Container';
+import { Chip } from '@mui/material'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
 
 const Car = (props) => {
+    const {id} = useParams()
+    const car = cars.find((car) => car.id.toString() === id) // Turns the car's ID as an identifiabe string.
+
     return (
-        <h1>A specific car</h1>
+        <Container maxWidth="md" sx={{ mt: 20}}>
+            <Card>
+                <CardContent sx={{m: 5}}>
+                    <h1>{car.Name}</h1>
+                    <Chip label={"ID: " + car.id}/>
+                    <Chip label={"Miles per Gallon: " + car.Miles_per_Gallon}/>
+                    <Chip label={"Cylinders: " + car.Cylinders}/>
+                    <Chip label={"Displacement: " + car.Displacement}/>
+                    <Chip label={"Horsepower: " + car.Horsepower}/>
+                    <Chip label={"Weight in lbs: " + car.Weight_in_lbs}/>
+                    <Chip label={"Acceleration: " + car.Acceleration}/>
+                    <Chip label={"Year: " + car.Year}/>
+                    <Chip label={"Origin: " + car.Origin}/>
+                </CardContent>
+            </Card>
+        </Container>
     )
 }
 
